14cb9d6ea8304a162015785ad5616fb2104ce907
RasenMedina <rasenmm10@gmail.com>
2026-09-16 14:38:54 +0200

    S'ha afegit el codi de TypseScript amb tsconfig i html

--- TypeScript/1r dia/Prova1.js
+++ TypeScript/1r dia/Prova1.js
@@ -1,3 +1,16 @@
 "use strict";
+// Introducció a TypeScript - Dimarts 15-9-26
+// Definim una variable de tipus string amb el missatge
 let missatge = "Jordi macu què tal? Trempat";
+// Mostrem el missatge per la consola del navegador (F12)
 console.log(missatge);
+// Mostrem una finestra emergent tipus alerta
+alert(missatge);
+// Busquem l'element HTML que té l'id "missatge"
+// Usem una comprovació perquè TypeScript sàpiga segur que l'element existeix
+let m = document.getElementById("missatge");
+if (m) {
+    // Si l'element existeix, hi injectem el text dins del HTML
+    m.innerHTML = missatge;
+}
+//# sourceMappingURL=Prova1.js.map
